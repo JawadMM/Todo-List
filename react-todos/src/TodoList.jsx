8d0db78e23607a3d10759fecf45f371e61bc46d8
JawadMM <Almuttawa.j@hotmail.com>
2023-08-28 20:49:37 +0300

    first commit

---
+++ react-todos/src/TodoList.jsx
@@ -0,0 +1,7 @@
+const initialTodos = [
+  {id: 1, text: "Read Chapter 1", completed: false},
+  {id: 2, text: "Read Chapter 2", completed: true},
+  {id: 3, text: "Read Chapter 3", completed: true},
+  {id: 4, text: "Read Chapter 4", completed: false},
+]
+
